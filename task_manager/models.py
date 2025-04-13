196242ee16f287193e913bb061ed577faaceb20b
tsurkanOdessa <set.tsurkan@gmail.com>
2025-04-13 15:02:51 +0200

    Initial migration

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    categories = models.ManyToManyField(Category, related_name='tasks')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['title', 'deadline']  # или 'title' и дата без времени
+
+    def __str__(self):
+        return self.title
+
+class SubTask(models.Model):
+    STATUS_CHOICES = Task.STATUS_CHOICES
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} (для задачи {self.task.title})"
