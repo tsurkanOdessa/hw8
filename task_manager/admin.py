e09c316b821f14776a7acba0a4b00d92490b7e27
tsurkanOdessa <set.tsurkan@gmail.com>
2025-04-13 15:11:05 +0200

    Initial admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Task, SubTask, Category
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Category)
